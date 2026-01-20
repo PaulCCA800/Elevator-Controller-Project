e0e928f782165616b27e10b2baa72c795cceb03a
Michal <michal2stan@gmail.com>
2026-01-20 10:46:16 +0100

    Start of exercise, start process from process

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::process::Command;
+
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let item = &args[1];
+    if item == "help"
+    {
+        let _ = Command::new("kitty")
+            .args(&["cargo", "run", "--", "else"])
+            .spawn()
+            .expect("Error Something Broke");
+    }
+    
+    loop
+    {
+        println!("Test");
+    }
 }
