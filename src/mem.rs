7a227662584a9d47706745bb053d7b981025d8e7
Paul Christian Cappelen <Paul Christian Cappelen>
2026-02-03 17:11:18 +0100

    functionality for writing to matrix

--- src/mem.rs
+++ src/mem.rs
@@ -1,9 +1,14 @@
+use std::{collections::HashMap, fmt::format, option}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
 enum Direction {
     Up,
     Down,
+    Stop,
 }
-
+#[derive(Clone)]
 struct Order {
+    id: u8,
     floor: u8,
     cab: bool,
     direction: Direction,
@@ -20,12 +25,13 @@ struct Elevator {
 }
 
 struct Matrix {
-    matrix: Vec<Elevator> 
+    matrix: HashMap <u8, Elevator> 
 }
 
 impl Order {
-    pub fn new(floor: u8, cab: bool, direction: Direction) -> Self{
+    pub fn new(id: u8, floor: u8, cab: bool, direction: Direction) -> Self{
         Self{
+            id,
             floor,
             cab,
             direction,
@@ -47,27 +53,76 @@ impl Elevator{
             hall_orders,
         }
     }
+
+    fn set_current_floor(&mut self, floor: u8) {
+        self.current_floor = floor;
+    }
+
+    fn set_direction(&mut self, dir: &Direction) {
+        self.direction = dir;
+    }
+
+    fn set_obstruction(&mut self, obs: &bool) {
+        self.obstruction = obs;
+    }
+
+    fn set_stop(&mut self, stop: &bool) {
+        self.stop = stop;
+    }
+
+    fn set_cab_orders(&mut self, orders: &[Order]) {
+        self.cab_orders = orders.to_vec();
+    }
+
+    fn set_hall_orders(&mut self, orders: &[Order]) {
+        self.hall_orders = orders.to_vec();
+    }
 }
 
 impl Matrix {
-    pub fn new(matrix: Vec<Elevator>) -> Self{
+    pub fn new(matrix: HashMap <u8, Elevator>) -> Self{
         Self {matrix}
     }
 
-    fn write_to_matrix(matrix: &mut Matrix, id: u8) {
-
+    fn get(&self, id: u8) -> &Elevator {
+        self.matrix.get(id).expect(&format!("get error: no elevator found at {id}.", id))
     }
 
-    fn read_from_matrix(matrix: &Matrix) {
+    fn get_mut(&mut self, id: u8) -> &mut Elevator {
+        self.matrix.get_mut(id).expect(&format!("get_mut error: no elevator found at {id}.", id))
+    }
 
-    } 
+    fn write_elev_current_floor(&mut self, id: u8, floor: u8) {
+        self.get_mut(id).set_current_floor(floor);
+    }
+
+    fn set_elev_direction(&mut self, id: u8, direction: &Direction) {
+        self.get_mut(id).set_direction(direction);
+    }
+
+    fn set_elev_obstruction(&mut self, id: u8, obstruction: &bool) {
+        self.get_mut(id).set_obstruction(obstruction);
+    }
+
+    fn set_elev_stop(&mut self, id: u8, stop: &bool) {
+        self.get_mut(id).set_stop(stop);
+    }
+
+    fn set_elev_cab_orders(&mut self, id: u8, orders: &[Order]) {
+        self.get_mut(id).set_cab_orders(orders);
+    }
+
+    fn set_elev_hall_orders(&mut self, id: u8, orders: &[Order]) {
+        self.get_mut(id).set_hall_orders(orders);
+    }
+
 }
 
-fn send_to_network() {
+fn send_to_channel() {
 
 }
 
-fn recv_from_network() {
+fn recv_from_channel() {
 
 }
 
