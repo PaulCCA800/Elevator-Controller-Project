56e76c86ea94cb601896eb2f8ba006aeb8105eb2
Michal <michal2stan@gmail.com>
2026-02-08 15:59:25 +0100

    Working basic control of elevator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::sync::{Mutex, Arc};
-use std::time;
+use std::time::{self, Duration};
 use std::thread::{self, JoinHandle};
 
 mod udpserver;
@@ -9,7 +9,13 @@ mod hardware;
 
 use crate::message::message::UdpMsg;
 use crate::udpserver::udp_server::Server;
-use crate::hardware::hardware::ElevatorData;
+
+use driver_rust::elevio;
+use crossbeam_channel as cbc;
+
+const LOCAL_ADDR: &str = "localhost:3030";
+const FLOOR_COUNT: u8 = 4;
+const POOL_DUR: Duration = Duration::from_millis(10);
 
 fn main() {
     let mut elevator_threads: Vec<JoinHandle<()>> = vec![];
@@ -70,14 +76,66 @@ fn main() {
     // Hardware Thread
     elevator_threads.push(thread::spawn(move ||
     {
-        let hardware_controller = ElevatorData::spawn();
+        let (call_button_tx, call_button_rx)    = cbc::unbounded::<elevio::poll::CallButton>(); 
+        let (floor_sensor_tx, floor_sensor_rx)                  = cbc::unbounded::<u8>(); 
+        let (stop_button_tx, stop_button_rx)                = cbc::unbounded::<bool>();
+        let (obstruction_tx, obstruction_rx)                = cbc::unbounded::<bool>(); 
+
+        let elevator = elevio::elev::Elevator::init(LOCAL_ADDR, FLOOR_COUNT).unwrap();
+
+        {
+            let elevator_call = elevator.clone();
+            thread::spawn(move || elevio::poll::call_buttons(elevator_call, call_button_tx, POOL_DUR));
+        }
+        
+        {
+            let elevator_floor = elevator.clone();
+            thread::spawn(move || elevio::poll::floor_sensor(elevator_floor, floor_sensor_tx, POOL_DUR));    
+        }
+
+        {
+            let elevator_stop = elevator.clone();    
+            thread::spawn(move || elevio::poll::stop_button(elevator_stop, stop_button_tx, POOL_DUR));
+        }
+
+        {
+            let elevator_obstruction = elevator.clone();    
+            thread::spawn(move || elevio::poll::obstruction(elevator_obstruction, obstruction_tx, POOL_DUR));
+        }
+
+        let mut direction: u8 = elevio::elev::DIRN_UP; 
+        elevator.motor_direction(direction);
 
         loop
         {
-            {
-
+            cbc::select!{
+                recv(call_button_rx) -> o => {
+                    let call_button = o.unwrap();
+                    println!("{:?}", call_button);
+                },
+                recv(floor_sensor_rx) -> o => {
+                    let floor_sensor = o.unwrap();
+                    println!("{:?}", floor_sensor);
+                    if floor_sensor == 3
+                    {
+                        direction = elevio::elev::DIRN_DOWN;
+                    }
+                    else if floor_sensor == 0
+                    {
+                        direction = elevio::elev::DIRN_UP;
+                    }
+                    elevator.motor_direction(direction);
+                },
+                recv(stop_button_rx) -> o => {
+                    let stop_button = o.unwrap();
+                    println!("{:?}", stop_button);
+                },
+                recv(obstruction_rx) -> o => {
+                    let obstruction = o.unwrap();
+                    println!("{:?}", obstruction);
+                }
             }
-            thread::sleep(time::Duration::from_millis(10));
+            thread::sleep(time::Duration::from_millis(100));
         }
     }));
 
